7c1932d672fa8da62821fe0a8795d30550f8f163
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2022-05-25 00:50:49 -0500

    25/05/2022

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include "librerias.h"
 
-int main(){
+void imprimirMatriz(int** ma,int n,int m){
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++)
+      cout<<ma[i][j]<<" ";
+    cout<<endl;
+  }
+};
 
-    return 0;
+
+void crear_matriz(int** ma,int n,int m){
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin>>ma[i][j]; 
+};
+
+
+int main(){
+    int n,m;
+    cin>>n;
+    cin>>m;
+    while(n <1 && m >1000){
+        cin>>n;
+        cin>>m;
+    }
+    int** matriz_especial = new int*[n];
+    for(int i=0; i<n; i++)
+         matriz_especial[i] = new int[m];
+    crear_matriz(matriz_especial,n,m);
+    int  minimo=matriz_especial[0][0];
+    vector<int>array_de_min;
+    int indice;
+    for(int i=0;i<m;i++){
+          for(int i=0; i<m;i++){
+        if(minimo>= matriz_especial[0][i]){
+            minimo=matriz_especial[0][i];
+            indice=i;
+        }
+    }
+    array_de_min.push_back(indice);
+    }
+  
+  
+
+    return 0;   
 }
