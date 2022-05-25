224da8f6639d685a64cb4144514d1f37ec928e54
XChanitoX <christian.salazar.a@utec.edu.pe>
2022-05-25 13:27:31 -0500

    Fixing Main

--- main.cpp
+++ main.cpp
@@ -6,42 +6,47 @@ void imprimirMatriz(int** ma,int n,int m){
       cout<<ma[i][j]<<" ";
     cout<<endl;
   }
-};
-
+}
 
 void crear_matriz(int** ma,int n,int m){
     for(int i=0; i<n; i++)
         for(int j=0; j<m; j++)
             cin>>ma[i][j]; 
-};
+}
 
 
 int main(){
     int n,m;
-    cin>>n;
-    cin>>m;
+    cout << "Ingrese el número de filas: ";
+    cin >> n;
+    cout << "Ingrese el número de columnas: ";
+    cin >> m;
+
     while(n <1 && m >1000){
-        cin>>n;
-        cin>>m;
+        cout << "Ingrese el número de filas: ";
+        cin >> n;
+        cout << "Ingrese el número de columnas: ";
+        cin >> m;
     }
+
     int** matriz_especial = new int*[n];
     for(int i=0; i<n; i++)
          matriz_especial[i] = new int[m];
     crear_matriz(matriz_especial,n,m);
-    int  minimo=matriz_especial[0][0];
+
+    int minimo = matriz_especial[0][0];
+
     vector<int>array_de_min;
     int indice;
     for(int i=0;i<m;i++){
-          for(int i=0; i<m;i++){
-        if(minimo>= matriz_especial[0][i]){
-            minimo=matriz_especial[0][i];
-            indice=i;
+          for(int j=0; j<m;j++){
+            if(minimo>= matriz_especial[0][j]){
+                minimo=matriz_especial[0][j];
+                indice=i;
         }
     }
     array_de_min.push_back(indice);
     }
-  
-  
 
     return 0;   
 }
